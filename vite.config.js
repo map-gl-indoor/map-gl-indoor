c2d5c9b029988459e709ea7d2b3783c0c69d886d
Thibaud <thibaud.michel@gmail.com>
2022-05-02 09:36:51 +0200

    chore: change vite's root directory to example #14

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    root: resolve(__dirname, './example'),
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
