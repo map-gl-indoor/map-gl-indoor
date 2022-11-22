228b24eb620acd67a916b4e263aeedb97ccc5ae6
Thibaud <thibaud.michel@gmail.com>
2022-11-22 10:04:52 +0100

    chore: rename example folder to examples

--- vite.examples.config.js
+++ vite.examples.config.js
@@ -3,17 +3,17 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { readdirSync } from 'fs';
 
-const examplePath = resolve(__dirname, './example');
+const examplesPath = resolve(__dirname, './examples');
 
-const dirCont = readdirSync( examplePath );
+const dirCont = readdirSync( examplesPath );
 const htmlFiles = dirCont
     .filter( ( elm ) => elm.match(/.*\.(html?)/ig))
-    .map(file => resolve(examplePath, file));
+    .map(file => resolve(examplesPath, file));
 
 export default defineConfig({
-    root: examplePath,
+    root: examplesPath,
     build: {
-        outDir: resolve(__dirname, 'dist', 'example'),
+        outDir: resolve(__dirname, 'dist', 'examples'),
         sourcemap: true,
         target: 'esnext',
         rollupOptions: {
