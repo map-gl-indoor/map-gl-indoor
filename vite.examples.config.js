ba88bac8c3dff3df8f77cef2ec9a3c94f3cdad72
Thibaud <thibaud.michel@gmail.com>
2022-11-22 12:28:29 +0100

    chore(build): add missing subfolders in examples

--- vite.examples.config.js
+++ vite.examples.config.js
@@ -1,23 +1,52 @@
 // vite.config.js
+
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { readdirSync } from 'fs';
+import { copyFileSync, existsSync, mkdirSync, readdirSync } from 'fs';
+
+function copyFiles(from, to, overwrite = false) {
+    return {
+        name: 'copy-files',
+        generateBundle() {
+            if (!existsSync(to)) mkdirSync(to, { recursive: true });
+            const log = msg => console.log('\x1b[36m%s\x1b[0m', msg)
+            log(`copy files: ${from} → ${to}`);
+            readdirSync(from).forEach(file => {
+                const fromFile = `${from}/${file}`;
+                const toFile = `${to}/${file}`;
+                if (existsSync(toFile) && !overwrite)
+                    return
+                log(`• ${fromFile} → ${toFile}`)
+                copyFileSync(
+                    resolve(fromFile),
+                    resolve(toFile)
+                )
+            })
+        }
+    }
+}
 
 const examplesPath = resolve(__dirname, './examples');
-
-const dirCont = readdirSync( examplesPath );
+const dirCont = readdirSync(examplesPath);
 const htmlFiles = dirCont
-    .filter( ( elm ) => elm.match(/.*\.(html?)/ig))
+    .filter((elm) => elm.match(/.*\.(html?)/ig))
     .map(file => resolve(examplesPath, file));
+
+const outDir = resolve(__dirname, 'dist', 'examples');
+
 
 export default defineConfig({
     root: examplesPath,
     build: {
-        outDir: resolve(__dirname, 'dist', 'examples'),
+        outDir,
         sourcemap: true,
         target: 'esnext',
         rollupOptions: {
-            input: htmlFiles
+            input: htmlFiles,
+            plugins: [
+                copyFiles(resolve(examplesPath, 'img'), resolve(outDir, 'img')),
+                copyFiles(resolve(examplesPath, 'maps'), resolve(outDir, 'maps'))
+            ]
         }
     }
 });
